376209d75c269eda6ee170348b38d9f8e7bc7b7a
David <dph0718@hotmail.com>
2018-02-18 23:49:39 -0700

    Removes prompts & alerts, tracks wins & losses, adds timeOut to game messages

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -66,6 +66,11 @@ var gallowsImages = [
 ]
 //iterates through gallows images
 var imageCounter = 0;
+//wins & losses
+var losses = 0;
+var wins = 0;
+
+var keyTimer;
 
 //gives a random integer between 0 and x
 var randomNum = function (x) {
@@ -89,6 +94,8 @@ function makeWordArray(y) {
 }
 //Checks if input is a letter, and if it's included in the selected word
 var checkLetter = function (letter) {
+    console.log(words.length);
+    console.log(gameState);
     letter = letter.toLowerCase();
     //checks against array of acceptable input
     if (letters.includes(letter)) {
@@ -99,68 +106,73 @@ var checkLetter = function (letter) {
                     wordLetterArray.splice(i, 1, "null");
                     wordArray.splice(i, 1, letter);
                     document.querySelector('#blanks').innerHTML = wordArray.join(" ");
+                    document.querySelector('#gameMessage').innerHTML = "Yep! Choose another letter.";
                 }
             }
             //adds the correct guess to the rightLetters array
             rightLetters.push(letter);
             return;
         } else if (guessedLetters.includes(letter)) {
-            alert("You've already tried that one.");
+            document.querySelector('#gameMessage').innerHTML = "You've already tried that one.";
             return;
         } else if (rightLetters.includes(letter)) {
-            alert("You've clearly already guessed that one.")
+            document.querySelector('#gameMessage').innerHTML = "You've clearly already guessed that one.";
             return;
         } else if (!wordLetterArray.includes(letter) && !guessedLetters.includes(letter)) {
             guessedLetters.push(letter);
             document.querySelector('#franklin').innerHTML = guessedLetters.join(", ");
-            imageCounter ++;
+            imageCounter++;
             document.querySelector('#gallows').src = gallowsImages[imageCounter];
-            alert("Nope!");
-            return;
+            document.querySelector('#gameMessage').innerHTML = "Nope! Try again!";
         }
-    //if it ain't a letter....
+        //if it ain't a letter....
     } else {
-        alert("Only letters, pal. Try again.")
+        document.querySelector('#gameMessage').innerHTML = "Only letters, pal. Try again.";
     }
 }
 //function, called back, to see if 'x' is 'null'
 var isNull = function (x) {
     return x === 'null';
 }
+
+//timeout for win/lose game message
+function pressKeyPrompt() {
+    keyTimer = setTimeout(function () {
+        document.querySelector('#gameMessage').innerHTML = "Press any key to play again.";
+        gameState = "not started"
+    }, 2000)
+}
+
 //Conditions for win/lose
 var checkWin = function () {
     //more than 6 guesses and you lose
     if (guessedLetters.length > 6) {
         document.querySelector('#gameMessage').innerHTML = "You lost. The word was &quot;" + word + ".&quot;";
+        losses++;
         gameState = "lose";
-    //but if every letter from the selected word has been replaced with 'null', you win.
+        document.querySelector('#losses').innerHTML = losses;
+        pressKeyPrompt();
+        detectStart();
+
+        //but if every letter from the selected word has been replaced with 'null', you win.
     } else if (wordLetterArray.every(isNull)) {
         document.querySelector('#gameMessage').innerHTML = "You win!";
+        wins++;
         gameState = "win";
+        document.querySelector('#wins').innerHTML = wins;
+        pressKeyPrompt();
+        detectStart();
     } else {
         gameState = "playing";
     }
-
 }
 //I don't think these need to be here, as they're called in start() when the game starts. delete?
 // word = pickWord();
 // makeWordArray(word);
 
 function playagain() {
-    if (gameState == "lose") {
-        again = confirm('So you lost. Give it another go?');
-    } else if (gameState == "win") {
-        again = confirm("You won! Play again?");
-    } 
-    if(again == true){
-        if (words.length == 0){
-            alert("Congratulations. You've made it through every word in the English language, give or take a few. Reload the page to guess the same words again.")
-        }
-        start();
-    }
-    else {
-        return;
-    }
+    pressKeyPrompt();
+    detectStart();
 }
 
 //--------Start Game Functions
@@ -176,23 +188,33 @@ detectStart = function () {
                 start();
             }
         } else {
-            return;
+            gameState = "not started";
         }
     }
 }
 
 function start() {
+    clearTimeout(keyTimer);
     document.querySelector('#gameMessage').innerHTML = "Choose any letter.";
+    document.querySelector('#wins').innerHTML = wins;
+    document.querySelector('#losses').innerHTML = losses;
     imageCounter = 0;
     document.querySelector('#gallows').src = gallowsImages[imageCounter];
     guessedLetters = [];
     wordArray = [];
     wordLetterArray = [];
     rightLetters = [];
-    word = pickWord();
-    makeWordArray(word);
-    gameState = 'playing';
-    playGame();
+    if (words.length == 0) {
+        document.querySelector('#gameMessage').innerHTML = "Congratulations. You've made it through every word in the English language, give or take a few. Reload the page to guess the same words again.";
+        document.querySelector('#blanks').innerHTML = "";
+        document.querySelector('#franklin').innerHTML = "";
+
+    } else {
+        word = pickWord();
+        makeWordArray(word);
+        gameState = 'playing';
+        playGame();
+    }
 }
 
 
@@ -209,30 +231,28 @@ function playGame() {
                 'wordarray ' + wordArray +
                 'wordletterarray ' + wordLetterArray +
                 'guessedletters ' + guessedLetters +
-                ' word length: ' + wordLength +
+                'word length: ' + wordLength +
                 'rightletters ' + rightLetters +
-                'emptyarray' + emptyArray +
-                'gameState' + gameState +
+                'emptyarray ' + emptyArray +
+                'gameState ' + gameState +
                 'input ' + input +
                 'again ' + again +
-                'word' + word);
+                'word ' + word);
 
             document.onkeydown = function () {
                 console.log(
-                    ' wordarray ' + wordArray +
-                    ' wordletterarray ' + wordLetterArray +
-                    ' guessedletters ' + guessedLetters +
-                    ' word length: ' + wordLength +
-                    ' rightletters ' + rightLetters +
-                    ' emptyarray' + emptyArray +
-                    ' gameState' + gameState +
-                    ' input ' + input +
-                    ' again ' + again +
-                    ' word' + word);
+                    'wordarray ' + wordArray +
+                    'wordletterarray ' + wordLetterArray +
+                    'guessedletters ' + guessedLetters +
+                    'word length: ' + wordLength +
+                    'rightletters ' + rightLetters +
+                    'emptyarray ' + emptyArray +
+                    'gameState ' + gameState +
+                    'input ' + input +
+                    'again ' + again +
+                    'word ' + word);
             }
-        } else if (gameState == "win") {
-            playagain();
-        } else if (gameState == "lose") {
+        } else if (gameState == "win" || "lose") {
             playagain();
         }
     }
